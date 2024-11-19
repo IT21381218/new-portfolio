85b076dec76f4e20b2489cd025ad0dc0c2c029a7
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-11-19 22:46:10 +0530

    m

--- my-portfolio/src/components/Top.jsx
+++ my-portfolio/src/components/Top.jsx
@@ -40,7 +40,7 @@ const Top = () => {
 
       // Apply movement based on gamma and beta
       const moveX = (gamma / 90) * 100; // Horizontal movement based on tilt <-- --> defual value =20
-      const moveY = (beta / 90) * 50;  // Vertical movement based on tilt      defual value =20
+      const moveY = (beta / 90) * -50;  // Vertical movement based on tilt      defual value =20
 
       if (textElement) {
         textElement.style.transform = `
