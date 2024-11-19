540a6121023ecded69b6632c4590214d1c92b13a
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-11-20 00:45:13 +0530

    Update Top.jsx

--- my-portfolio/src/components/Top.jsx
+++ my-portfolio/src/components/Top.jsx
@@ -35,12 +35,12 @@ const Top = () => {
       const { gamma, beta } = event; // Gamma (left-right tilt) and Beta (up-down tilt)
 
       // Apply rotation based on gamma and beta
-      const rotateX = beta / 2; // Up/Down rotation  defual value =2  (i made -2)
-      const rotateY = gamma / 2; // Left/Right rotation   defual value =2  (i made 2)
+      const rotateX = beta / 10; // Up/Down rotation  defual value =2  (i made -2)
+      const rotateY = gamma / 10; // Left/Right rotation   defual value =2  (i made 2)
 
       // Apply movement based on gamma and beta
-      const moveX = (gamma / 90) * 20; // Horizontal movement based on tilt <-- --> defual value =20   (i made 50)
-      const moveY = (beta / 90) * 20;  // Vertical movement based on tilt    /\ \/ defual value =20   (i made -20)
+      const moveX = (gamma / 90) * -20; // Horizontal movement based on tilt <-- --> defual value =20   (i made 50)
+      const moveY = (beta / 90) * -20;  // Vertical movement based on tilt    /\ \/ defual value =20   (i made -20)
 
       if (textElement) {
         textElement.style.transform = `
